84815a0e3f42ec605932e0129a5d30b0320fc247
Martin Cornel <oumartinc@gmail.com>
2016-08-16 10:28:50 +0300

    Fibonacci
    The initial fibonacci python

---
+++ fib.py
@@ -0,0 +1,12 @@
+def fib(limit):
+    results=[]
+    last=1
+    slast=0
+    while last < limit:
+        results.append(last)
+        new = last+slast
+        slast=last
+        last=new
+    return results
+
+print(fib(10))
